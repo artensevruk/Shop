3e27026a48e5cab1d30b5f549401c9b7feeb0c0d
Artem <sevruk04@bk.ru>
2024-03-19 21:11:05 +0300

    delete import

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './index.css';
 import {Header} from "./components/Header"
 import {Basement} from "./components/Basement"
 import { Outlet } from 'react-router-dom';
-import { getDataBasket } from './getDataBasket';
-import { getData } from './getData';
 import {
   QueryClient,
   QueryClientProvider
