4de300203b8f9c16f22de1eddaafb8c6bf52b4c4
Artem <sevruk04@bk.ru>
2024-03-19 22:13:05 +0300

    Изменили структуру кода в app  (есть проблема)

--- src/api.js
+++ src/api.js
@@ -1,32 +1,33 @@
-export const deleteBasket = (id) => {
-  return fetch(`http://localhost:8081/cartProduct/${id}`, {
-    method: "DELETE", // Здесь так же могут быть GET, PUT, DELETE
+const fetchData = async (url, method, data) => {
+  const options = {
+    method,
     headers: {
-      // Добавляем необходимые заголовки
       "Content-type": "application/json; charset=UTF-8",
     },
-  })
+  };
+  
+  if (data) {
+    options.body = JSON.stringify(data);
+  }
+
+  const response = await fetch(url, options);
+  return response.json();
 };
 
+const host = "http://localhost:8081"
 
-export const addBasket = (product) =>{
-  fetch('http://localhost:8081/carts', {
-  method: 'POST', // Здесь так же могут быть GET, PUT, DELETE
-  body: JSON.stringify(product), // Тело запроса в JSON-формате
-  headers: {
-    // Добавляем необходимые заголовки
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-}
+export const deleteBasket = (id) => {
+  return fetchData(`${host}/cartProduct/${id}`, "DELETE");
+};
 
-export const getData = async () =>{
-  const response  = await fetch('http://localhost:8081/products')
-  return response.json()
- }
+export const addBasket = (product) => {
+  return fetchData(`${host}/carts`, 'POST', product);
+};
 
+export const getData = async () => {
+  return fetchData(`${host}/products`, 'GET');
+};
 
- export const getDataBasket = async () =>{
-  const response  = await fetch('http://localhost:8081/cartProduct')
-  return response.json()
- }
+export const getDataBasket = async () => {
+  return fetchData(`${host}/cartProduct`, 'GET');
+};
